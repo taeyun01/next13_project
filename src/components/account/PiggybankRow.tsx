91250a4e81349cbe98f2a409d40bb8bccc1e5828
taeyun <lwe12f675@gmail.com>
2025-02-26 21:23:28 +0900

    feat: 계좌 상세 페이지 - 생성한 저금통 정보를 불러와 렌더링

--- src/components/account/PiggybankRow.tsx
+++ src/components/account/PiggybankRow.tsx
@@ -3,8 +3,61 @@ import { css } from '@emotion/react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
+import withSuspense from '@/hooks/withSuspense'
+
+import { getPiggybank } from '@/remote/piggybank'
+import { useQuery } from '@tanstack/react-query'
+import useUser from '@/hooks/useUser'
+import { differenceInDays } from 'date-fns'
+import Flex from '@/components/shared/Flex'
+import Text from '@/components/shared/Text'
+import addDelimiter from '@/utils/addDelimiter'
+
 const PiggybankRow = () => {
   const navigate = useRouter()
+  const user = useUser()
+
+  const { data: piggybank } = useQuery({
+    queryKey: ['piggybank', user?.id],
+    queryFn: () => getPiggybank(user?.id as string),
+    suspense: true,
+  })
+
+  console.log('piggybank', piggybank)
+
+  // 생성한 저금통이 없는 경우
+  if (!piggybank) {
+    return (
+      <div>
+        <ul>
+          <ListRow
+            style={piggybankRowStyles}
+            left={
+              <Image
+                src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-24-1024.png"
+                alt="piggybank"
+                width={40}
+                height={40}
+              />
+            }
+            contents={
+              <ListRow.ListRowTexts
+                title="저금통"
+                subTitle="매일 매일 조금씩 저금하여 목표금액을 모아보아요!"
+              />
+            }
+            withArrow
+            onClick={() => navigate.push('/account/piggybank/new')}
+          />
+        </ul>
+      </div>
+    )
+  }
+
+  const { balance, endDate, goalAmount } = piggybank
+  const dday = differenceInDays(endDate, new Date()) // 종료일과 현재 날짜의 D-day
+
+  // 생성한 저금통이 있는 경우
   return (
     <div>
       <ul>
@@ -19,13 +72,17 @@ const PiggybankRow = () => {
             />
           }
           contents={
-            <ListRow.ListRowTexts
-              title="저금통"
-              subTitle="매일 매일 조금씩 저금하여 목표금액을 모아보아요!"
-            />
+            <Flex direction="column">
+              <Text typography="t4" bold>
+                D-{dday}
+              </Text>
+              <Text>{addDelimiter(goalAmount - balance)}원 남았어요!</Text>
+            </Flex>
           }
           withArrow
-          onClick={() => navigate.push('/account/piggybank/new')}
+          onClick={() => {
+            //TODO : 저금통 상세 페이지 이동 or 저금통 리스트 페이지 이동
+          }}
         />
       </ul>
     </div>
@@ -36,4 +93,6 @@ const piggybankRowStyles = css`
   cursor: pointer;
 `
 
-export default PiggybankRow
+export default withSuspense(PiggybankRow, {
+  fallback: <div>로딩중...</div>,
+})
--- src/remote/piggybank.ts
+++ src/remote/piggybank.ts
@@ -1,8 +1,43 @@
 import { Piggybank } from '@/types/piggybank'
-import { collection, doc, setDoc } from 'firebase/firestore'
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore'
 import { store } from '@/remote/firebase'
 import { COLLECTIONS } from '@/constants/collection'
 
 export const createPiggybank = (newPiggybank: Piggybank) => {
   return setDoc(doc(collection(store, COLLECTIONS.PIGGYBANK)), newPiggybank)
+}
+
+export const getPiggybank = async (userId: string) => {
+  const querySnapshot = await getDocs(
+    query(
+      collection(store, COLLECTIONS.PIGGYBANK),
+      where('userId', '==', userId),
+      where('endDate', '>=', new Date()),
+      orderBy('endDate', 'asc'), // 가장 최근순
+      limit(1),
+    ),
+  )
+
+  if (querySnapshot.docs.length === 0) {
+    return null
+  }
+
+  // 문서의 첫번째 요소를 가져옴
+  const piggybank = querySnapshot.docs[0].data()
+
+  return {
+    id: querySnapshot.docs[0].id,
+    ...(piggybank as Piggybank),
+    startDate: piggybank.startDate.toDate(), // 파이어 베이스에서 Date를 타임스태프로 바꿔버리기 때문에 다시 가지고 와서 Date 타입으로 변경해줘야함
+    endDate: piggybank.endDate.toDate(),
+  }
 }
