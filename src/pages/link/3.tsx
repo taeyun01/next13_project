a5cea4567e97cab02d31c2af8a84264c11f1a694
taeyun <lwe12f675@gmail.com>
2025-03-01 23:23:55 +0900

    feat: useRouter로 prefetch하는 방법

--- src/pages/link/3.tsx
+++ src/pages/link/3.tsx
@@ -1,12 +1,26 @@
 import Spacing from '@/components/shared/Spacing'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Link3 = () => {
+  const router = useRouter()
+
+  const moveTo1Page = () => {
+    router.push('/link/1')
+  }
+
+  useEffect(() => {
+    router.prefetch('/link/1')
+  }, [router])
+
   return (
     <div>
       Link3 Page
       <Spacing size={1000} />
       <Link href="/link/1">Link1로 이동</Link>
+      <Spacing size={20} />
+      <div onClick={moveTo1Page}>Link1로 이동</div>
     </div>
   )
 }
