72a7276588c7c90c89fd8857730d233cb8de31f2
taeyun <lwe12f675@gmail.com>
2025-03-10 09:02:44 +0900

    feat: 목표금액 통장 페이지 - 진행중인 통장, 종료된 통장 필터 하여 렌더링

--- src/pages/account/piggybank/detail.tsx
+++ src/pages/account/piggybank/detail.tsx
@@ -2,30 +2,116 @@ import Top from '@/components/shared/Top'
 import { getPiggybankList } from '@/remote/piggybank'
 import useUser from '@/hooks/useUser'
 import { useQuery } from '@tanstack/react-query'
+import ListRow from '@/components/shared/ListRow'
+import Image from 'next/image'
+import { differenceInDays } from 'date-fns'
+import Button from '@/components/shared/Button'
+import { useRouter } from 'next/router'
 
 const PiggybankDetail = () => {
   const user = useUser()
+  const navigate = useRouter()
 
   const { data: piggybankList } = useQuery({
     queryKey: ['piggybankList', user?.id],
     queryFn: () => getPiggybankList(user?.id as string),
     suspense: true,
+    enabled: !!user?.id, // 유저가 있을때만 쿼리 실행
   })
 
-  // console.log('user', user?.id)
+  if (!piggybankList) {
+    return <div>통장이 없습니다.</div>
+  }
 
-  console.log('piggybankList', piggybankList)
+  // 목표 날짜가 진행중인 통장 필터
+  const currentPiggybank = piggybankList.filter((piggybank) => {
+    const endDate = new Date(piggybank.endDate)
+    return endDate > new Date()
+  })
+
+  // 목표 날짜가 끝난 통장 필터
+  const endedPiggybankList = piggybankList.filter((piggybank) => {
+    const endDate = new Date(piggybank.endDate)
+    return endDate < new Date()
+  })
 
   return (
     <div>
-      {/* //TODO : 진행중인 목표 통장 리스트 구현하기 */}
-      <Top
-        title="진행중인 통장"
-        subtitle="매일 매일 조금씩 저금하여 목표금액을 모아보아요!"
-      />
+      <Top title="진행중인 통장" subtitle="" />
+      <ul>
+        {currentPiggybank.length === 0 ? (
+          <ListRow
+            left={
+              <Image
+                src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-24-1024.png"
+                alt="piggybank"
+                width={40}
+                height={40}
+              />
+            }
+            contents={
+              <ListRow.ListRowTexts
+                title="진행중인 통장이 없습니다."
+                subTitle=""
+              />
+            }
+          />
+        ) : (
+          currentPiggybank.map((piggybank) => (
+            <ListRow
+              key={piggybank.id}
+              left={
+                <Image
+                  src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-24-1024.png"
+                  alt="piggybank"
+                  width={40}
+                  height={40}
+                />
+              }
+              contents={
+                <ListRow.ListRowTexts
+                  title={piggybank.name}
+                  subTitle={`D-${differenceInDays(piggybank.endDate, new Date())}`}
+                />
+              }
+              right={
+                <Button
+                  color="basic"
+                  onClick={() => {
+                    // TODO: 저금하기 페이지로 이동
+                    navigate.push(`/account/piggybank/detail/${piggybank.id}`)
+                  }}
+                >
+                  저금하기
+                </Button>
+              }
+            />
+          ))
+        )}
+      </ul>
 
-      {/* //TODO : 종료된 목표 통장 리스트 구현하기 */}
       <Top title="종료된 통장" subtitle="" />
+      <ul>
+        {endedPiggybankList.map((piggybank) => (
+          <ListRow
+            key={piggybank.id}
+            left={
+              <Image
+                src="https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-24-1024.png"
+                alt="piggybank"
+                width={40}
+                height={40}
+              />
+            }
+            contents={
+              <ListRow.ListRowTexts
+                title={piggybank.name}
+                subTitle={`${new Date(piggybank.startDate).toLocaleDateString()} ~ ${new Date(piggybank.endDate).toLocaleDateString()}`}
+              />
+            }
+          />
+        ))}
+      </ul>
     </div>
   )
 }
